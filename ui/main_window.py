b96ba460fc923501ee7756ebdc099e5b94da866b
KupeRr <nikita.ssau@gmail.com>
2021-12-06 17:38:05 +0400

    Add sketch of main window

--- ui/main_window.py
+++ ui/main_window.py
@@ -1,36 +1,41 @@
-import sys
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
-from PyQt5.QtCore import *
+from tkinter import *
 
-
-class Window(QWidget):
-
-    def __init__(self):
-        super().__init__()
-
-        self.init_ui()
-
-
-    def init_ui(self):
-
-        self.setGeometry(300, 300, 600, 450)
-        self.setWindowTitle('*SomeName*')
-
-        self.label = QLabel('Welcome to helper *Name*!')
-        self.label.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        self.label.setAlignment(Qt.AlignCenter)
-        self.label.setStyleSheet("QLabel {background-color: red;}")
-        
-
-        self.show()
-
-    def init_welcome_window():
-        pass
-
+class Checkbar(Frame):
+     def __init__(self, parent=None, picks=[], side=LEFT, anchor=W):
+        Frame.__init__(self, parent)
+        self.vars = []
+        for pick in picks:
+            var = IntVar()
+            chk = Checkbutton(self, text=pick, variable=var)
+            chk.pack(side=side, anchor=anchor, expand=YES)
+            self.vars.append(var)
+        def state(self):
+            return map((lambda var: var.get()), self.vars)
 
 if __name__ == '__main__':
+    root = Tk()
+    root.title('*Name*')
+    #root.geometry('450x300')
+   
+    label_del = Label(root, text='Выберите фирму доставки').pack(side=TOP, fill=X)
 
-    app = QApplication(sys.argv)
-    ex = Window()
-    sys.exit(app.exec_())
+    delivery = Checkbar(root, ['Boxberry', 'CDEK'])
+    delivery.pack(side=TOP,  fill=X)
+    delivery.config(relief=GROOVE, bd=2)
+
+    label_city = Label(root, text='Выберите зону доставки').pack(side=TOP, fill=X)
+
+    city_zone = Checkbar(root, ['1', '2', '3', 'Один город'])
+    city_zone.pack(side=TOP,  fill=X)
+    city_zone.config(relief=GROOVE, bd=2)
+
+
+    def allstates(): 
+        print(list(delivery.state()))
+        print(list(city_zone.state()))
+
+    Button(root, text='Выйти', command=root.quit).pack(side=RIGHT)
+    Button(root, text='Загрузить', command=allstates).pack(side=RIGHT)
+
+    root.resizable(width=False, height=False)
+    root.mainloop()
