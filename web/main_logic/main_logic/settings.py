17ab6130092d4767ac32499b793e09579f9a56af
KupeRr <nikita.ssau@gmail.com>
2021-12-19 18:27:22 +0400

    Secret key logic fix

--- web/main_logic/main_logic/settings.py
+++ web/main_logic/main_logic/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-dir_path = pathlib.Path.cwd().parent.parent
-content = open(str(dir_path) + r'\.config').read()
+content = open(str(BASE_DIR.parent.parent) + r'\.config').read()
 config  = eval(content)
 
 # SECURITY WARNING: keep the secret key used in production secret!
